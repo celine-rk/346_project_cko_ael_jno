ad88c80cda27530b025ee4bf5408c80149354d26
Celine Koenig <celine.koenig@hostpoint.ch>
2023-12-15 15:34:00 +0100

    Sicherheitsgruppen ID's abgefragt beider Gruppen

--- ec2instances/initialize_instances.sh
+++ ec2instances/initialize_instances.sh
@@ -29,6 +29,7 @@ KEY_NAME="wordpress-key"
 WP_SECURITY_GROUP="wp-sec-group"
 DB_SECURITY_GROUP="db-sec-group"
 
+
 # Name für die Datenbank-Instanz und die Webserver-Instanz
 DB_INSTANCE_NAME="DB-Wordpress"
 WP_INSTANCE_NAME="WP-Webserver"
@@ -71,33 +72,40 @@ aws ec2 create-security-group --group-name "$WP_SECURITY_GROUP" --description "E
 aws ec2 authorize-security-group-ingress --group-name "$WP_SECURITY_GROUP" --protocol tcp --port 80 --cidr 0.0.0.0/0  # HTTP-Verbindung erlauben
 aws ec2 authorize-security-group-ingress --group-name "$WP_SECURITY_GROUP" --protocol tcp --port 22 --cidr 0.0.0.0/0  # SSH-Verbindung erlauben
 
+# Gruppenid der webserver sicherheitsgruppe herausfinden
+WP_SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --group-names $WP_SECURITY_GROUP --query 'SecurityGroups[0].GroupId' --output text)
+
 # Sicherheitsgruppen für die Datenbank-Instanz erstellen
 aws ec2 create-security-group --group-name "$DB_SECURITY_GROUP" --description "EC2-database"
 aws ec2 authorize-security-group-ingress --group-name "$DB_SECURITY_GROUP" --protocol tcp --port 3306 --source-group "$WP_SECURITY_GROUP"
+
+# Gruppenid für die Datenbankinstanz herausfinden
+DB_SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --group-names $DB_SECURITY_GROUP --query 'SecurityGroups[0].GroupId' --output text)
+
 
 # Elastic Network Interface (ENI) für die Datenbank-Instanz erstellen
 ENI_ID=$(aws ec2 create-network-interface --subnet-0fcde7cae9536c1ab --private-ip-address "$DB_PRIVATE_IP" --query 'NetworkInterface.NetworkInterfaceId' --output text)
 
 # AWS EC2-Datenbankinstanz erstellen und ENI zuweisen
-aws ec2 run-instances --region "$REGION" --image-id "$IMAGE_ID" --instance-type "$INSTANCE_TYPE" --key-name "$KEY_NAME" --security-group "$DB_SECURITY_GROUP" --network-interfaces "NetworkInterfaceId=$ENI_ID,DeviceIndex=0" --user-data file://cloudconfig-db.yaml --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$DB_INSTANCE_NAME}]" 
+aws ec2 run-instances --region "$REGION" --image-id "$IMAGE_ID" --instance-type "$INSTANCE_TYPE" --key-name "$KEY_NAME" --security-group-ids "$DB_SECURITY_GROUP_ID" --network-interfaces "NetworkInterfaceId=$ENI_ID,DeviceIndex=0" --user-data file://cloudconfig-db.yaml --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$DB_INSTANCE_NAME}]" 
 
 # ID der Datenbankinstanz abrufen
 DB_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$DB_INSTANCE_NAME" --query 'Reservations[0].Instances[0].InstanceId' --output text --region "$REGION")
 
 # Warten, bis die Datenbankinstanz läuft
-aws ec2 wait instance-running --instance-ids "$DB_INSTANCE_ID" --region "$REGION"
+#aws ec2 wait instance-running --instance-ids "$DB_INSTANCE_ID" --region "$REGION"
 
 # Private IP der Datenbankinstanz abrufen
 DB_INTERNAL_IP=$(aws ec2 describe-instances --instance-ids "$DB_INSTANCE_ID" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text --region "$REGION")
 
 # AWS Webserver/WordPress-Instanz erstellen
-aws ec2 run-instances --region "$REGION" --image-id "$IMAGE_ID" --instance-type "$INSTANCE_TYPE" --key-name "$KEY_NAME" --security-group "$WP_SECURITY_GROUP" --user-data file://cloudconfig-web.yaml --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$WP_INSTANCE_NAME}]"
+aws ec2 run-instances --region "$REGION" --image-id "$IMAGE_ID" --instance-type "$INSTANCE_TYPE" --key-name "$KEY_NAME" --security-group-ids "$WP_SECURITY_GROUP_ID" --user-data file://cloudconfig-web.yaml --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$WP_INSTANCE_NAME}]"
 
 # ID der Webserverinstanz abrufen
 WP_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$WP_INSTANCE_NAME" --query 'Reservations[0].Instances[0].InstanceId' --output text --region "$REGION")
 
 # Warten, bis die Webserverinstanz läuft
-aws ec2 wait instance-running --instance-ids "$WP_INSTANCE_ID" --region "$REGION"
+#aws ec2 wait instance-running --instance-ids "$WP_INSTANCE_ID" --region "$REGION"
 
 # Öffentliche IP der Webserverinstanz abrufen
 WPPUBLICIP=$(aws ec2 describe-instances --instance-ids "$WP_INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text --region "$REGION")
