5374b6318a9eb8c19490419b1f618fd1275f1db1
Celine Koenig <celine.koenig@hostpoint.ch>
2023-12-21 20:26:52 +0100

    ID Übergabe Sec-Groups vereinfacht + Korrigiert

--- ec2instances/initialize_instances.sh
+++ ec2instances/initialize_instances.sh
@@ -67,80 +67,79 @@ if [[ "$ANSWER_CONTINUE" =~ [nN] ]]; then
     exit 1
 fi
 
-# Erstellen des SSH-Schlüsselpaars
-echo -e "Creating a new SSH key-pair..."
-echo "aws ec2 create-key-pair --key-name "$KEY_NAME" --key-type rsa --query "KeyMaterial" --output text > "$KEY_NAME.pem""
+# # Erstellen des SSH-Schlüsselpaars
+# echo -e "Creating a new SSH key-pair..."
+# aws ec2 create-key-pair --key-name "$KEY_NAME" --key-type rsa --query "KeyMaterial" --output text > "$KEY_NAME.pem"
 
-# Neues VPC erstellen und deren ID in Variable VPC_ID hinzufügen
-echo -e "create $GREEN new vpc $NOCOLOR....."
+# # Neues VPC erstellen und deren ID in Variable VPC_ID hinzufügen
+# echo -e "create $GREEN new vpc $NOCOLOR....."
 
-VPC_ID=$(aws ec2 create-vpc --cidr-block "$CIDR_BLOCK" --query 'Vpc.VpcId' --output text)
+# VPC_ID=$(aws ec2 create-vpc --cidr-block "$CIDR_BLOCK" --query 'Vpc.VpcId' --output text)
 
-# Neues Subnet erstellen
-echo -e "creating $GREEN new subnet $NOCOLOR....."
-SUBNET_ID=$(aws ec2 create-subnet --vpc-id "$VPC_ID" --cidr-block "$CIDR_BLOCK" --query 'Subnet.SubnetId' --output text)
+# # Neues Subnet erstellen
+# echo -e "creating $GREEN new subnet $NOCOLOR....."
+# SUBNET_ID=$(aws ec2 create-subnet --vpc-id "$VPC_ID" --cidr-block "$CIDR_BLOCK" --query 'Subnet.SubnetId' --output text)
 
-# Sicherheitsgruppen für die Webserver-Instanz erstellen (VPC_ID hinzufügen daher verschoben)
- echo -e "Creating $GREEN webserver security group $NOCOLOR..."
- aws ec2 create-security-group --group-name "$WP_SECURITY_GROUP" --description "EC2-wordpress --vpc-id "$VPC_ID""
- aws ec2 authorize-security-group-ingress --group-name "$WP_SECURITY_GROUP" --protocol tcp --port 80 --cidr 0.0.0.0/0  # HTTP-Verbindung erlauben
- aws ec2 authorize-security-group-ingress --group-name "$WP_SECURITY_GROUP" --protocol tcp --port 22 --cidr 0.0.0.0/0  # SSH-Verbindung erlauben
+# # Sicherheitsgruppen für die Webserver-Instanz erstellen (id anstelle von namen übergeben)
+# echo -e "Creating $GREEN webserver security group $NOCOLOR..."
+# WP_SECURITY_GROUP_ID=$(aws ec2 create-security-group --group-name "$WP_SECURITY_GROUP" --description "EC2-wordpress" --vpc-id "$VPC_ID" --query 'GroupId' --output text)
+# aws ec2 authorize-security-group-ingress --group-id "$WP_SECURITY_GROUP_ID" --protocol tcp --port 80 --cidr 0.0.0.0/0  # HTTP-Verbindung erlauben
+# aws ec2 authorize-security-group-ingress --group-id "$WP_SECURITY_GROUP_ID" --protocol tcp --port 22 --cidr 0.0.0.0/0  # SSH-Verbindung erlauben
 
-#  Gruppenid der webserver sicherheitsgruppe herausfinden
-WP_SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --group-names $WP_SECURITY_GROUP --query 'SecurityGroups[0].GroupId' --output text)
 
-#  Sicherheitsgruppen für die Datenbank-Instanz erstellen
+# #  Sicherheitsgruppen für die Datenbank-Instanz erstellen 
 echo -e "Creating $GREEN database security group $NOCOLOR..."
-aws ec2 create-security-group --group-name "$DB_SECURITY_GROUP" --description "EC2-database --vpc-id "$VPC_ID""
-aws ec2 authorize-security-group-ingress --group-name "$DB_SECURITY_GROUP" --protocol tcp --port 3306 --source-group "$WP_SECURITY_GROUP"
-aws ec2 authorize-security-group-ingress --group-name "$DB_SECURITY_GROUP" --protocol tcp --port 22 --source-group "$WP_SECURITY_GROUP"
-
-# Gruppenid für die Datenbankinstanz herausfinden
-DB_SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --group-names "$DB_SECURITY_GROUP" --query 'SecurityGroups[0].GroupId' --output text)
-
-# Elastic Network Interface (ENI) für die Datenbank-Instanz erstellen
-echo -e "Creating $GREEN network interface for database instance $NOCOLOR..."
-
-ENI_ID_DB=$(aws ec2 create-network-interface --subnet-id "$SUBNET_ID" --private-ip-address "$DB_PRIVATE_IP" --groups "$DB_SECURITY_GROUP_ID" --query 'NetworkInterface.NetworkInterfaceId' --output text)
-
-# Elastic Network Interface (ENI) für die Datenbank-Instanz erstellen
-echo -e "Creating $GREEN network interface for webserver instance $NOCOLOR..."
-ENI_ID_WEB=$(aws ec2 create-network-interface --subnet-id "$SUBNET_ID" --groups "$WP_SECURITY_GROUP_ID" --associate-public-ip-address --query 'NetworkInterface.NetworkInterfaceId' --output text)
+VPC_ID="vpc-0753053a90303adba"
+echo "$VPC_ID"
+echo "DB_SECURITY_GROUP_ID=$(aws ec2 create-security-group --group-name "$DB_SECURITY_GROUP" --description "EC2-database" --vpc-id "$VPC_ID" --query 'GroupId' --output text)"
+echo "$DB_SECURITY_GROUP_ID"
+aws ec2 authorize-security-group-ingress --group-id "$DB_SECURITY_GROUP_ID" --protocol tcp --port 3306 
+aws ec2 authorize-security-group-ingress --group-id "$DB_SECURITY_GROUP_ID" --protocol tcp --port 22 
 
 
-# AWS EC2-Datenbankinstanz erstellen und ENI zuweisen
-echo -e "Creating $GREEN database instance $NOCOLOR..."
-aws ec2 run-instances --region "$REGION" --image-id "$IMAGE_ID" --instance-type "$INSTANCE_TYPE" --key-name "$KEY_NAME" --network-interfaces "NetworkInterfaceId="$ENI_ID_DB",DeviceIndex=0" --user-data file://cloudconfig-db.yaml --no-associate-public-ip-address --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$DB_INSTANCE_NAME}]"
+# # Elastic Network Interface (ENI) für die Datenbank-Instanz erstellen
+# echo -e "Creating $GREEN network interface for database instance $NOCOLOR..."
+# SUBNET_ID="subnet-0259268a4b29d63bf"
+# ENI_ID_DB=$(aws ec2 create-network-interface --subnet-id "$SUBNET_ID" --private-ip-address "$DB_PRIVATE_IP" --groups "$DB_SECURITY_GROUP_ID" --query 'NetworkInterface.NetworkInterfaceId' --output text)
 
-# ID der Datenbankinstanz abrufen
-DB_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$DB_INSTANCE_NAME" --query 'Reservations[0].Instances[0].InstanceId' --output text --region "$REGION")
-
-# Warten, bis die Datenbankinstanz läuft
-aws ec2 wait instance-running --instance-ids "$DB_INSTANCE_ID" --region "$REGION"
-
-# AWS Webserver/WordPress-Instanz erstellen und Network interface zu weisen
-aws ec2 run-instances --region "$REGION" --image-id "$IMAGE_ID" --instance-type "$INSTANCE_TYPE" --key-name "$KEY_NAME" --network-interfaces "NetworkInterfaceId="$ENI_ID_WEB",DeviceIndex=0" --user-data file://cloudconfig-web.yaml --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$WP_INSTANCE_NAME}]"
-
-# ID der Webserverinstanz abrufen
-WP_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$WP_INSTANCE_NAME" --query 'Reservations[0].Instances[0].InstanceId' --output text --region "$REGION")
-
-# Warten, bis die Webserverinstanz läuft
-aws ec2 wait instance-running --instance-ids "$WP_INSTANCE_ID" --region "$REGION"
-
-# Öffentliche IP der Webserverinstanz abrufen
-WPPUBLICIP=$(aws ec2 describe-instances --instance-ids "$WP_INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text --region "$REGION")
-
-# Ende und Ausgabe der öffentlichen IP zur Wordpress Seite
-echo -e "Die Reihenfolge der Befehle ist wichtig! Die Verbindung zwischen der Webserver-Instanz und der DB-Instanz kann nur geprüft werden, wenn eine $GREEN SSH-Verbindung $NOCOLOR auf den $GREEN Webserver $NOCOLOR erfolgte"
+# # Elastic Network Interface (ENI) für die Datenbank-Instanz erstellen
+# echo -e "Creating $GREEN network interface for webserver instance $NOCOLOR..."
+# ENI_ID_WEB=$(aws ec2 create-network-interface --subnet-id "$SUBNET_ID" --groups "$WP_SECURITY_GROUP_ID" --associate-public-ip-address --query 'NetworkInterface.NetworkInterfaceId' --output text)
 
 
-echo -e "WordPress-Instanz erstellt. Öffne $GREEN http://"$WPPUBLICIP" $NOCOLOR im Browser, um die Konfiguration abzuschließen."
+# # AWS EC2-Datenbankinstanz erstellen und ENI zuweisen
+# echo -e "Creating $GREEN database instance $NOCOLOR..."
+# aws ec2 run-instances --region "$REGION" --image-id "$IMAGE_ID" --instance-type "$INSTANCE_TYPE" --key-name "$KEY_NAME" --network-interfaces "NetworkInterfaceId="$ENI_ID_DB",DeviceIndex=0" --user-data file://cloudconfig-db.yaml --no-associate-public-ip-address --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$DB_INSTANCE_NAME}]"
 
-# SSH Verbindungen zu Instanzen
-echo -e "Eine Verbindung zur Webserver-Instanz via ssh kann wie folgt vorgenommen werden: ssh -i $KEY_NAME.pem ubuntu@$WPPUBLICIP"
+# # ID der Datenbankinstanz abrufen
+# DB_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$DB_INSTANCE_NAME" --query 'Reservations[0].Instances[0].InstanceId' --output text --region "$REGION")
 
-# Datenbank Verbindung zu Webserver herstellen reihenfolge wechseln
-echo -e "Datenbank-Instanz wurde erstellt. Über folgenden Befehl kann die Kommunikation von Webserver zu DB geprüft werden: $BOLD mysql -h  $GREEN $DB_PRIVATE_IP $NOCOLOR -u $GREEN wpuser $NOCOLOR -p $REGULAR" 
-echo -e "Das PW für $BOLD wpuser $REGULAR lautet: $BOLD X4#L6LwrN4V!w4&m^6pH98Li $REGULAR " 
+# # Warten, bis die Datenbankinstanz läuft
+# aws ec2 wait instance-running --instance-ids "$DB_INSTANCE_ID" --region "$REGION"
 
-echo -e "Eine Verbindung zur Datenbank-Instanz via ssh kann wie folgt vorgenommen werden: ssh -i $KEY_NAME.pem ubuntu@$DB_PRIVATE_IP"
+# # AWS Webserver/WordPress-Instanz erstellen und Network interface zu weisen
+# aws ec2 run-instances --region "$REGION" --image-id "$IMAGE_ID" --instance-type "$INSTANCE_TYPE" --key-name "$KEY_NAME" --network-interfaces "NetworkInterfaceId="$ENI_ID_WEB",DeviceIndex=0" --user-data file://cloudconfig-web.yaml --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$WP_INSTANCE_NAME}]"
+
+# # ID der Webserverinstanz abrufen
+# WP_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$WP_INSTANCE_NAME" --query 'Reservations[0].Instances[0].InstanceId' --output text --region "$REGION")
+
+# # Warten, bis die Webserverinstanz läuft
+# aws ec2 wait instance-running --instance-ids "$WP_INSTANCE_ID" --region "$REGION"
+
+# # Öffentliche IP der Webserverinstanz abrufen
+# WPPUBLICIP=$(aws ec2 describe-instances --instance-ids "$WP_INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text --region "$REGION")
+
+# # Ende und Ausgabe der öffentlichen IP zur Wordpress Seite
+# echo -e "Die Reihenfolge der Befehle ist wichtig! Die Verbindung zwischen der Webserver-Instanz und der DB-Instanz kann nur geprüft werden, wenn eine $GREEN SSH-Verbindung $NOCOLOR auf den $GREEN Webserver $NOCOLOR erfolgte"
+
+
+# echo -e "WordPress-Instanz erstellt. Öffne $GREEN http://"$WPPUBLICIP" $NOCOLOR im Browser, um die Konfiguration abzuschließen."
+
+# # SSH Verbindungen zu Instanzen
+# echo -e "Eine Verbindung zur Webserver-Instanz via ssh kann wie folgt vorgenommen werden: ssh -i $KEY_NAME.pem ubuntu@$WPPUBLICIP"
+
+# # Datenbank Verbindung zu Webserver herstellen reihenfolge wechseln
+# echo -e "Datenbank-Instanz wurde erstellt. Über folgenden Befehl kann die Kommunikation von Webserver zu DB geprüft werden: $BOLD mysql -h  $GREEN $DB_PRIVATE_IP $NOCOLOR -u $GREEN wpuser $NOCOLOR -p $REGULAR" 
+# echo -e "Das PW für $BOLD wpuser $REGULAR lautet: $BOLD X4#L6LwrN4V!w4&m^6pH98Li $REGULAR " 
+
+# echo -e "Eine Verbindung zur Datenbank-Instanz via ssh kann wie folgt vorgenommen werden: ssh -i $KEY_NAME.pem ubuntu@$DB_PRIVATE_IP"
